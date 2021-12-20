36c31f5bda407e58c63f0d70a225b6e04ca145ca
ADMIN\Admin <vukhai27091998@gmail.com>
2021-12-20 11:51:34 +0700

    IntoKeyword Work With CsvFile

---
+++ LinqBasic/IntoKeyword/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IntoKeyWord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IList<Student> studentList = new List<Student>() {
+        new Student() { StudentID = 1, StudentName = "John", Age = 13 } ,
+        new Student() { StudentID = 2, StudentName = "Steve",   Age = 15 } ,
+        new Student() { StudentID = 3, StudentName = "Bill",   Age = 18 } ,
+        new Student() { StudentID = 4, StudentName = "Ram" ,  Age = 12 } ,
+        new Student() { StudentID = 5, StudentName = "Ron" ,  Age = 21 }
+        };
+            var teenAgerStudents = from s in studentList
+                                   where s.Age > 12 && s.Age < 20
+                                   select s
+                                  into teenStudent
+                                   where teenStudent.StudentName.StartsWith("B")
+                                   select teenStudent;
+            foreach (var student in teenAgerStudents)
+            {
+                Console.WriteLine("Name: {0} - Age:{1} ", student.StudentName, student.Age);
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ LinqBasic/WorkWithCvsFile/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace WorkWithCvsFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<string> strCsv = File.ReadAllLines("Sample.csv");
+            var result = from str in strCsv
+                         let tmp = str.Split(",").Skip(1).Select(s => Convert.ToInt32(s))
+                         select new
+                         {
+                             Max = tmp.Max(),
+                             Min = tmp.Min(),
+                             Total = tmp.Sum(),
+                             Agv = tmp.Average()
+
+                         };
+            var query = result.ToList();
+            foreach (var item in query)
+            {
+                Console.WriteLine(
+                    string.Format($"Maxxium: {item.Max} Minxium: {item.Min} Total: {item.Total} Average: {item.Agv}"));
+            }
+            Console.Read();
+                      
+        }
+    }
+}
+ 
