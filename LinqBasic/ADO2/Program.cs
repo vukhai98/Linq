f58ee99c41dd9d43277c4549a3090b4be541bb90
ADMIN\Admin <vukhai27091998@gmail.com>
2021-12-24 17:12:15 +0700

    DataAdapter WDF work with Adapter

--- LinqBasic/ADO2/Program.cs
+++ LinqBasic/ADO2/Program.cs
@@ -1,17 +1,45 @@
 ﻿using System;
 using System.Data;
-using System.Data.Common;
 using System.Data.SqlClient;
+
 
 namespace ADO2
 {
     class Program
     {
+        static void ShowDataTable(DataTable table)
+        {
+            Console.WriteLine($"NameofTable: {table.TableName}");
+            Console.Write($"Index",10);
+
+            foreach (DataColumn c in table.Columns)
+            {
+                Console.Write($"{c.ColumnName,20}");
+
+            }
+            Console.WriteLine();
+            int index = 0;
+            int numberColumns = table.Columns.Count;
+            foreach (DataRow r in table.Rows)
+            {
+                Console.Write($"{index}",10);
+                for (int i = 0; i < numberColumns; i++)
+                {
+
+                    Console.Write($"{r[i],20}");
+                }
+                Console.WriteLine();
+                index++;
+             
+
+            }
+
+        }
         static void Main(string[] args)
         {
             // kết nối với SQL Server 
             string stringSqlConnection = "Server=ADMIN;Database=Employees;Trusted_Connection=True";
-                             
+
             var connection = new SqlConnection(stringSqlConnection);
             //Console.WriteLine(connection.State);
             connection.Open();
@@ -61,8 +89,9 @@ namespace ADO2
             //Console.WriteLine(returnValue);
             #endregion
             //command.NonQuery(); - khi Update,Insert,Delete thì nó sẽ trả ra số dòng bị tác động bởi câu lệnh
-            using var command = new SqlCommand();
-            command.Connection = connection;
+            #region ExcuteNonQuery
+            //using var command = new SqlCommand();
+            //command.Connection = connection;
             //command.CommandText = " insert into  Skills (Title) values(@value)";
             //var value = command.Parameters.AddWithValue("@value","");
             //for (int i = 0; i < 4; i++)
@@ -71,22 +100,80 @@ namespace ADO2
             //}
             //var result = command.ExecuteNonQuery();
             //Console.WriteLine(result);
-            command.CommandText = "getinfo";
-            command.CommandType = CommandType.StoredProcedure;
-            var id = new SqlParameter("@id", 0);
-            command.Parameters.Add(id);
-            id.Value = 2;
-            var reader = command.ExecuteReader();
-            if (reader.HasRows)
-            {
-                reader.Read();
-                var ID = reader[0];
-                var Name = reader[1];
-                var Title = reader[2];
-                Console.WriteLine($"  {ID}  :  {Name} : {Title} ");
-                    
-            }
+            #endregion
+            #region StoredProcedure
+            //using var command = new SqlCommand();
+            //command.Connection = connection;
+            //command.CommandText = "getinfo";
+            //command.CommandType = CommandType.StoredProcedure;
+            //var id = new SqlParameter("@id", 0);
+            //command.Parameters.Add(id);
+            //id.Value = 2;
+            //var reader = command.ExecuteReader();
+            //if (reader.HasRows)
+            //{
+            //    reader.Read();
+            //    var ID = reader[0];
+            //    var Name = reader[1];
+            //    var Title = reader[2];
+            //    Console.WriteLine($"  {ID}  :  {Name} : {Title} ");
 
+            //}
+            #endregion
+
+            #region Create Table = Dataset;
+            //var dataset = new DataSet();
+            //using var table = new DataTable("My Table");
+            //dataset.Tables.Add(table);
+
+            //table.Columns.Add("Id");
+            //table.Columns.Add("Name");
+            //table.Columns.Add("Age");
+
+            //table.Rows.Add(1, "Peter Cech", 38);
+            //table.Rows.Add(2, "Mason Mount", 22);
+            //table.Rows.Add(3, "N'Golo Kante", 30);
+
+            //ShowDataTable(table);
+            #endregion
+            var adapter = new SqlDataAdapter();
+            adapter.TableMappings.Add("Table", "Employees");
+            // Select Command 
+            adapter.SelectCommand = new SqlCommand("Select EmployeeID,EmployeeName,PhoneNumber from Employees;", connection);
+            // Insert Command
+            adapter.InsertCommand = new SqlCommand("insert into Employees (EmployeeName,PhoneNumber) values (@EmployeeName,@PhoneNumber)", connection);
+            adapter.InsertCommand.Parameters.Add("@EmployeeName", SqlDbType.VarChar, 50, "EmployeeName");
+            adapter.InsertCommand.Parameters.Add("@PhoneNumber", SqlDbType.VarChar, 50, "PhoneNumber");
+            // Delete Command
+            adapter.DeleteCommand = new SqlCommand("delete from Employees where EmployeeID = @EmployeeID",connection);
+            var pr1 = adapter.DeleteCommand.Parameters.Add(new SqlParameter("@EmployeeID", SqlDbType.Int));
+            pr1.SourceColumn = "EmployeeID";
+            pr1.SourceVersion = DataRowVersion.Original;
+            adapter.UpdateCommand = new SqlCommand("update  Employees set EmployeeName = @EmployeeName where EmployeeID = @EmployeeID", connection);
+            var pr2 = adapter.UpdateCommand.Parameters.Add(new SqlParameter("@EmployeeID", SqlDbType.Int));
+            pr2.SourceColumn = "EmployeeID";
+            pr2.SourceVersion = DataRowVersion.Original;
+            adapter.UpdateCommand.Parameters.Add("@EmployeeName", SqlDbType.VarChar, 50, "EmployeeName");
+         
+
+            var dataSet = new DataSet();
+            adapter.Fill(dataSet);
+            DataTable table = dataSet.Tables["Employees"];
+            ShowDataTable(table);
+
+
+            //var row = table.Rows.Add();
+
+            //row["EmployeeName"] = "Didia Drogba";
+            //row["PhoneNumber"] = "9961166012";
+            //var row6 = table.Rows[6];
+            //row6.Delete();
+            //var r = table.Rows[5];
+            //r["EmployeeName"] = "Mason Mount";
+
+
+            adapter.Update(dataSet);
+            connection.Close();
         }
     }
 }
---
+++ LinqBasic/TestAdapter/MainWindow.xaml.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TestAdapter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DataTable table = new DataTable("Employees");
+        SqlConnection connection;
+        SqlDataAdapter adapter;
+        DataSet dataSet = new DataSet();
+        public MainWindow()
+        {
+            InitAdapter();
+            InitializeComponent();
+        }
+        void InitAdapter()
+        {
+            string stringSqlConnection = "Server=ADMIN;Database=Employees;Trusted_Connection=True";
+
+            connection = new SqlConnection(stringSqlConnection);
+            adapter = new SqlDataAdapter();
+            adapter.TableMappings.Add("Table", "Employees");
+            // Select Command 
+            adapter.SelectCommand = new SqlCommand("Select EmployeeID,EmployeeName,PhoneNumber from Employees;", connection);
+            // Insert Command
+            adapter.InsertCommand = new SqlCommand("insert into Employees (EmployeeName,PhoneNumber) values (@EmployeeName,@PhoneNumber)", connection);
+            adapter.InsertCommand.Parameters.Add("@EmployeeName", SqlDbType.VarChar, 50, "EmployeeName");
+            adapter.InsertCommand.Parameters.Add("@PhoneNumber", SqlDbType.VarChar, 50, "PhoneNumber");
+            // Delete Command
+            adapter.DeleteCommand = new SqlCommand("delete from Employees where EmployeeID = @EmployeeID", connection);
+            var pr1 = adapter.DeleteCommand.Parameters.Add(new SqlParameter("@EmployeeID", SqlDbType.Int));
+            pr1.SourceColumn = "EmployeeID";
+            pr1.SourceVersion = DataRowVersion.Original;
+            adapter.UpdateCommand = new SqlCommand("update  Employees set EmployeeName = @EmployeeName where EmployeeID = @EmployeeID", connection);
+            var pr2 = adapter.UpdateCommand.Parameters.Add(new SqlParameter("@EmployeeID", SqlDbType.Int));
+            pr2.SourceColumn = "EmployeeID";
+            pr2.SourceVersion = DataRowVersion.Original;
+            adapter.UpdateCommand.Parameters.Add("@EmployeeName", SqlDbType.VarChar, 50, "EmployeeName");
+
+            dataSet.Tables.Add(table);
+            //var dataSet = new DataSet();
+            //adapter.Fill(dataSet);
+            //DataTable table = dataSet.Tables["Employees"];
+            ////ShowDataTable(table);
+
+
+            ////var row = table.Rows.Add();
+
+            ////row["EmployeeName"] = "Didia Drogba";
+            ////row["PhoneNumber"] = "9961166012";
+            ////var row6 = table.Rows[6];
+            ////row6.Delete();
+            ////var r = table.Rows[5];
+            ////r["EmployeeName"] = "Mason Mount";
+
+
+            //adapter.Update(dataSet);
+            //connection.Close();
+
+
+        }
+        void LoadDataTable()
+        {
+            table.Rows.Clear();
+            adapter.Fill(table);
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            LoadDataTable();
+            datagrid.DataContext = table.DefaultView;
+        }
+
+        private void btnLoad_Click(object sender, RoutedEventArgs e)
+        {
+            LoadDataTable();
+            datagrid.DataContext = table.DefaultView;
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            adapter.Update(dataSet);
+            table.Rows.Clear();
+            adapter.Fill(dataSet);
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            DataRowView selectedItem = (DataRowView)datagrid.SelectedItem;
+            if (selectedItem != null)
+            {
+                selectedItem.Delete();
+            }
+        }
+    }
+    
+}
